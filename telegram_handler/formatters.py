f1565cdb3cf4a813a77a8bd68268409dac8754d4
Ilya Makhrov <ilya.makhrov@gmail.com>
2018-10-25 20:22:03 +0300

    Update telegram_handler/formatters.py
    Add proper exception message formating

--- telegram_handler/formatters.py
+++ telegram_handler/formatters.py
@@ -52,6 +52,15 @@ class HtmlFormatter(TelegramFormatter):
             record.name = escape_html(str(record.name))
         if record.msg:
             record.msg = escape_html(record.getMessage())
+
+        exception = ''
+        if record.exc_text:
+            exception = '\n' + record.exc_text
+        if record.stack_info:
+            exception = exception + '\n' + self.formatStack(record.stack_info)
+        if exception:
+            record.message = record.message + ('\n<pre>%s\n</pre>' % escape_html(exception))
+
         if self.use_emoji:
             if record.levelno == logging.DEBUG:
                 record.levelname += ' ' + EMOJI.WHITE_CIRCLE
@@ -61,7 +70,3 @@ class HtmlFormatter(TelegramFormatter):
                 record.levelname += ' ' + EMOJI.RED_CIRCLE
 
         return self.fmt % record.__dict__
-
-    def formatException(self, *args, **kwargs):
-        string = super(HtmlFormatter, self).formatException(*args, **kwargs)
-        return '<pre>%s</pre>' % escape_html(string)
